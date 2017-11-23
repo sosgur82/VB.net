c1e68a684286dac6f80e803a2f182af08c6c7ae7
sosgur82 <sosgur82@gamil.com>
2017-11-23 16:21:19 +0900

    초기 커밋.

---
+++ 20171123/Module1.vb
@@ -0,0 +1,22 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim intD As Integer
+        intD = 5
+
+        Console.WriteLine()
+        Console.WriteLine("실 인수 : {0}", intD)
+        GuGuDan(intD)
+    End Sub
+
+    Public Sub GuGuDan(intDan As Integer)
+        Console.WriteLine("실 인수 : {0}", intDan)
+
+        Dim intCount As Integer
+
+        For intCount = 1 To 9
+            Console.WriteLine("{0} * {1} = {2}", intDan, intCount, intDan * intCount)
+        Next
+    End Sub
+
+End Module
---
+++ ConsoleApp1/Module1.vb
@@ -0,0 +1,28 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim intValue1, intvalue2 As Integer
+
+        intValue1 = 10
+        intvalue2 = 20
+
+        Console.WriteLine()
+        Console.WriteLine("call by Value")
+        Console.WriteLine("Value1 = {0}, Value = {1}", intValue1, intvalue2)
+
+        Swap(intValue1, intvalue2)
+
+        Console.WriteLine("After swap")
+        Console.WriteLine("Value1 = {0}, Value = {1}", intValue1, intvalue2)
+    End Sub
+
+    Sub Swap(ByVal value1 As Integer, ByVal value2 As Integer)
+
+        Dim intTemp As Integer
+
+        intTemp = value1
+        value1 = value2
+        value2 = intTemp
+    End Sub
+
+End Module
